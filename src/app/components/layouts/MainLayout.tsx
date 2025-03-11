afd1cc97ecd186f39588460403774426543f3f35
JonBabao <jonendrickb@gmail.com>
2025-03-11 23:32:18 +0800

    Create Main Layout and reusable buttons

---
+++ src/app/components/layouts/MainLayout.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from 'react';
+
+const MainLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+    return(
+        <div>
+            <main>{children}</main>
+        </div>
+    );
+};
+
+export default MainLayout;
---
+++ src/app/styles/redButton.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+interface ButtonProperties {
+    children: React.ReactNode;
+    onClick?: () => void;
+    style?: React.CSSProperties;
+    type?: 'button' | 'submit' | 'reset';
+    disabled?: boolean; 
+}
+
+const RedButton: React.FC<ButtonProperties> = ({ children, onClick, style, type = 'button', disabled = false }) => {
+    return (
+        <button
+            type={type}
+            onClick={onClick}
+            disabled={disabled}
+            className="rematchRed text-gray-200 font-bold rounded-lg py-4 px-6 flex items-center justify-center hover:bg-[#2d2d2d] focus:outline-none disabled:opacity-50 disabled:cursor-not-allowed"
+            style={style}
+        >
+            {children}
+        </button>
+    );
+};
+
+export default RedButton;
