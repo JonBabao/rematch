b74b8bcc451756b222dc12cf313ea86b79b1f09b
JonBabao <jonendrickb@gmail.com>
2025-03-11 23:31:47 +0800

    Create Home page and component

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,10 @@
+import Login from '../../components/Login';
+import MainLayout from '@/app/components/layouts/MainLayout';
+
+export default function LoginPage() {
+    return(
+        <div>
+            <Login />
+        </div>
+    );
+}
---
+++ src/app/components/Home.tsx
@@ -0,0 +1,13 @@
+"use client";
+ 
+import React from 'react';
+
+const HomePage: React.FC = () => {
+    return(
+        <div>
+            <p>You are now inside.</p>
+        </div>
+    );
+};
+
+export default HomePage;
