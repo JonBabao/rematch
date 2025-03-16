fa13b7d0bb61d809340565eec5f96b13b119a063
JonBabao <jonendrickb@gmail.com>
2025-03-16 14:28:12 +0800

    Update Home.tsx to display LostItemsList

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -1,11 +1,14 @@
 "use client";
  
 import React from 'react';
+import LostItemsList from './LostItemsList';
 
 const HomePage: React.FC = () => {
     return(
-        <div>
-            <p>You are now inside.</p>
+        <div className="flex flex-col w-full">
+            <div className="flex flex-col bg-white mt-32 mx-10 rounded-lg p-10  ">
+                <LostItemsList />
+            </div>
         </div>
     );
 };
