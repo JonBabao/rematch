51c1f8e9cc3a5b1fa43c9aca0ca6aa8e2dc95138
JonBabao <jonendrickb@gmail.com>
2025-03-16 14:29:50 +0800

    Add TopNav to MainLayout

--- src/app/components/layouts/MainLayout.tsx
+++ src/app/components/layouts/MainLayout.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import React from 'react';
+import TopNav from '../TopNav'
 
 const MainLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     return(
         <div>
+            <TopNav />
             <main>{children}</main>
         </div>
     );
