bb1fbcc30db66f1c951f199b3fe6ce96db848ffe
JonBabao <jonendrickb@gmail.com>
2025-03-18 22:51:29 +0800

    Update UI for Home

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -6,7 +6,7 @@ import LostItemsList from './LostItemsList';
 const HomePage: React.FC = () => {
     return(
         <div className="flex flex-col w-full">
-            <div className="flex flex-col bg-white mt-32 mx-10 rounded-lg p-10  ">
+            <div className="flex flex-col bg-white mt-32 mb-16 mx-4 lg:mx-10 rounded-lg py-10 px-4 py-10 lg:px-10">
                 <LostItemsList />
             </div>
         </div>
