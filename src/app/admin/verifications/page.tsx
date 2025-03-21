be68aa286e461cc3ebf33a25d7be8d6379c4a801
JonBabao <jonendrickb@gmail.com>
2025-03-22 04:30:21 +0800

    Create page.tsx for AdminVerifications

---
+++ src/app/admin/verifications/page.tsx
@@ -0,0 +1,10 @@
+import AdminVerifications from '../../components/Verifications';
+import MainLayout from '../../components/layouts/MainLayout'
+
+export default function VerificationPage() {
+    return(
+        <MainLayout>
+            <AdminVerifications />
+        </MainLayout>
+    );
+}
