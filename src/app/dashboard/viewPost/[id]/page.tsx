e76fca13adee2ddb1b71b10e51ddbe4f46ab1af0
JonBabao <jonendrickb@gmail.com>
2025-03-21 20:31:59 +0800

    Create page.tsx for ViewPost

---
+++ src/app/dashboard/viewPost/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ViewPost from "../../../components/ViewPost";
+import MainLayout from "../../../components/layouts/MainLayout";
+
+export default function ViewPostPage() {
+    return(
+        <MainLayout>
+            <ViewPost />
+        </MainLayout>
+    );
+}
