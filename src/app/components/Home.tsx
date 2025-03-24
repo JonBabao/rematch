fdaf82ada3d0969fa9fe3c057d32382462cb4804
JonBabao <jonendrickb@gmail.com>
2025-03-24 22:55:04 +0800

    Improve UI of Home.tsx

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -5,8 +5,8 @@ import LostItemsList from './LostItemsList';
 
 const HomePage: React.FC = () => {
     return(
-        <div className="flex flex-col w-full">
-            <div className="flex flex-col bg-white mt-32 mb-16 mx-4 lg:mx-10 rounded-lg py-10 px-4 py-10 lg:px-10">
+        <div className="flex flex-col bg-[#f5f5f5] w-full">
+            <div className="flex flex-col mt-16 mb-16 mx-4 lg:mx-10 rounded-lg py-10 px-4 py-10 lg:px-10">
                 <LostItemsList />
             </div>
         </div>
